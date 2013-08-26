bc3ae1d7702dcc01ffa091068ac6fda1d17ff96e
unknown <fisers@LCANB722.dds.dillards.net>
2013-08-26 16:08:40 -0500

    moved set basics to module

--- set.rb
+++ set.rb
@@ -1,4 +1,7 @@
+require "./set_basics"
+
 class Set
+	include SetBasics
 	attr_accessor :values
 
 	def initialize *args
@@ -9,43 +12,4 @@ class Set
 			end
 		end
 	end
-
-	def intersect set
-		intersectionSet = Set.new
-		set.values.each do |elem|
-			if @values.include? elem
-				intersectionSet.values << elem
-			end
-		end
-		intersectionSet
-	end
-
-	def unite set
-		unionSet = Set.new
-		@values.each do |i|
-			unionSet.values << i
-		end
-		set.values.each do |j|
-			unless unionSet.values.include? j
-				unionSet.values << j
-			end
-		end
-		unionSet
-	end
-
-	def toss set
-		universal = Set.new(1,2,3,4,5,6,7,8,9,10)
-		intersect(set.complement(universal))
-	end
-
-	def complement set
-		compSet = set
-		@values.each do |elem|
-			if compSet.values.include? elem
-				compSet.values.delete(elem)
-			end
-		end
-		compSet
-	end
-
 end
---
+++ set_basics.rb
@@ -0,0 +1,39 @@
+module SetBasics
+	def intersect set
+		intersectionSet = Set.new
+		set.values.each do |elem|
+			if @values.include? elem
+				intersectionSet.values << elem
+			end
+		end
+		intersectionSet
+	end
+
+	def unite set
+		unionSet = Set.new
+		@values.each do |i|
+			unionSet.values << i
+		end
+		set.values.each do |j|
+			unless unionSet.values.include? j
+				unionSet.values << j
+			end
+		end
+		unionSet
+	end
+
+	def complement set
+		compSet = set
+		@values.each do |elem|
+			if compSet.values.include? elem
+				compSet.values.delete(elem)
+			end
+		end
+		compSet
+	end
+	
+	def toss set
+		universal = Set.new(1,2,3,4,5,6,7,8,9,10)
+		intersect(set.complement(universal))
+	end
+end
